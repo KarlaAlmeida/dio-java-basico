39ce0c0f8a5b46e34ecb854c62e036d4966bd535
KarlaAlmeida <karlakcbam@gmail.com>
2025-03-11 22:55:41 -0300

    Add classe MinhaClasse

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,9 @@
+public class MinhaClasse {
+
+    //Método para classe executável, inializa o projeto
+    //void - não retorna nada
+    public static void main(String[] args) {
+        System.out.print ("Olá! Sou Karla Almeida");
+    }
+
+}
