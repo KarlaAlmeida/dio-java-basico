05ece77eb1254db952bc72583e83eebe2bf6797d
KarlaAlmeida <karlakcbam@gmail.com>
2025-03-12 22:17:09 -0300

    Excluir arquivos testes

--- java-anatomia-classes/src/MinhaClasse.java
+++
@@ -1,9 +0,0 @@
-public class MinhaClasse {
-
-    //Método para classe executável, inializa o projeto
-    //void - não retorna nada
-    public static void main(String[] args) {
-        System.out.print ("Olá! Sou Karla Almeida");
-    }
-
-}
